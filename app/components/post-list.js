c64229328ca04bf4cef1f2e46782167d45c59a2a
Charlie Baxter and Dylan Barnard <charlie+dylan@>
2016-08-17 16:45:36 -0700

    add pagination using ember-cli-pagination

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Component.extend({
   tagName: 'ul',
   sortedPosts: Ember.computed.sort('posts', 'sortDefinition'),
-  sortDefinition: ['date'],
+  sortDefinition: ['date:desc'],
+  queryParams: ["page", "perPage"],
+  page: 1,
+  perPage: 5,
+  pagedContent:pagedArray('sortedPosts', {pageBinding: "page", perPageBinding:"perPage"}),
+  totalPagesBinding: "pagedContent.totalPages"
 });
